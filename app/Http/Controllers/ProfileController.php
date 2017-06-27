0badb89ceaf78d4d0dffd813d79780c54ae218f3
Chupa.Chups <gusinacenis88@gmail.com>
2017-06-27 18:40:48 +0200

    Step 7: Profile Routes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
     public function index($username)
     {
         $user = User::where('username', $username)->first();
-        return $user;
+        return view('user.profile');
 
     }
 }
