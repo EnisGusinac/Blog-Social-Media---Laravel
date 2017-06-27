4ed2fa61e9fe5e63201a76c0a3c8b39bb5ac42db
Chupa.Chups <gusinacenis88@gmail.com>
2017-06-27 17:35:52 +0200

    Step 3: First letter Capitalized - ucfirst

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,8 +63,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'username' => $data['username'],
+            'name' => ucfirst($data['name']),
+            'username' => ucfirst($data['username']),
             'dob' => $data['dob'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
