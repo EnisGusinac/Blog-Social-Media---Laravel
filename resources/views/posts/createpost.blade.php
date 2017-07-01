0fdb4cb62639fecc7c02d597c8729b963657af87
Chupa.Chups <gusinacenis88@gmail.com>
2017-07-01 21:00:02 +0200

    Textarea again in function

--- resources/views/posts/createpost.blade.php
+++ resources/views/posts/createpost.blade.php
@@ -12,10 +12,9 @@
 
                     <span class="w3-right w3-opacity">{{ $user->created_at->format('l j F Y') }}</span>
                     <h6 class="w3-opacity">Create your post:</h6>
-                    {{--<textarea resize="none" name="content" cols="82" rows="2" class="w3-border w3-padding-small">--}}
-                    {{--</textarea>--}}
+                    <textarea name="content" class="w3-border w3-padding-small"></textarea>
 
-                    <input style="width: 100%; height: 10%;" name="content" class="w3-border w3-padding-small">
+                    {{--<input style="width: 100%; height: 10%;" name="content" class="w3-border w3-padding-small">--}}
                 <button type="submit" class="w3-button w3-theme"><i class="fa fa-pencil"></i> &nbsp;Post</button>
 
                     {{--<form enctype="multipart/form-data" action="/posts" method="POST">--}}
