5dada135ed3987e3cc1e9c8da008707af6a8a9ba
Chupa.Chups <gusinacenis88@gmail.com>
2017-07-05 18:17:20 +0200

    Creating Admins Multi/Auth

---
+++ app/Admin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'username', 'dob', 'avatar'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // For natural function of dob
+    protected $dates = [
+        'dob', 'post_on'
+    ];
+}
