6a7b554a9808ab4a0f1df0137c5f734b8f4cb681
Chupa.Chups <gusinacenis88@gmail.com>
2017-07-07 21:34:32 +0200

    Print user posts

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -34,19 +34,15 @@
     <div class="w3-twothird w3-right">
 
         <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
-            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
-            <div class="w3-container">
-                <h5 class="w3-opacity"><b>Front End Developer / w3schools.com</b></h5>
-                <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-                <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                <hr>
-            </div>
-            <div class="w3-container">
-                <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-                <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-                <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                <hr>
-            </div>
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>User posts</h2>
+            @forelse($user->posts()->get() as $post)
+                <div class="w3-container">
+                    <p>{{ $post->content }}</p>
+                    <hr>
+                </div>
+            @empty
+                <p>User has no posts yet.</p>
+            @endforelse
         </div>
     </div>
     </div>
