f191aeb987ea9a8f22c1a72247287ef073b3fc0e
Chupa.Chups <gusinacenis88@gmail.com>
2017-07-21 10:20:59 +0200

    Reset password new form

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,99 +1,109 @@
-@extends('layouts.app')
 
-@section('content')
+        <!DOCTYPE html>
+<head>
+    <title>Forgot Password</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+    <link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
+</head>
+<body class="templatemo-bg-gray">
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="col-md-12">
+        <h1 class="margin-bottom-15">Password Reset</h1>
+        <form class="form-horizontal templatemo-forgot-password-form templatemo-container" role="form" action="{{ route('password.email') }}" method="post">
+            {{ csrf_field() }}
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+            <input type="hidden" name="token" value="{{ $token }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="form-group">
+                <div class="col-md-12">
+                    Please enter your email address that you registered in our website.
                 </div>
             </div>
-        </div>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <div for="email" class="col-md-12">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Your email"  value="{{ $email or old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-12">
+                    <input type="submit" value="Submit" class="btn btn-danger">
+                    <br><br>
+                    <a href="{{ route('login') }}">Go to Login</a>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
-@endsection
+</body>
+</html>
 
 
+{{--@section('content')--}}
+{{--<div class="container">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-heading">Reset Password</div>--}}
+                {{--<div class="panel-body">--}}
+                    {{--@if (session('status'))--}}
+                        {{--<div class="alert alert-success">--}}
+                            {{--{{ session('status') }}--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
 
-{{--<!DOCTYPE html>--}}
-{{--<html>--}}
+                    {{--<form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">--}}
+                        {{--{{ csrf_field() }}--}}
 
-{{--Login style--}}
-{{--<link rel="stylesheet" href="css/loginstyle.css">--}}
-
-{{--<body>--}}
-
-{{--<div id="id01" class="modal">--}}
-
-    {{--<div class="panel-body">--}}
-        {{--@if (session('status'))--}}
-            {{--<div class="alert alert-success">--}}
-                {{--{{ session('status') }}--}}
-            {{--</div>--}}
-        {{--@endif--}}
-
-            {{--<div class="container">--}}
-
-                {{--<form class="modal-content animate"  role="form" method="POST" action="{{ route('password.email') }}">--}}
-                {{--{{ csrf_field() }}--}}
-
-                    {{--<div class="col-md-6">--}}
-
-                        {{--<input type="hidden" name="token" value="{{ $token }}">--}}
                         {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
+                            {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
 
-                        {{--<label for="email"><b>E-Mail Address</b></label>--}}
-                        {{--<input id="email" type="email" placeholder="Enter e-mail" name="email" value="{{ old('email') }}" required>--}}
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>--}}
 
-                        {{--@if ($errors->has('email'))--}}
-                            {{--<span class="help-block">--}}
+                                {{--@if ($errors->has('email'))--}}
+                                    {{--<span class="help-block">--}}
                                         {{--<strong>{{ $errors->first('email') }}</strong>--}}
                                     {{--</span>--}}
-                        {{--@endif--}}
-                    {{--</div>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<div class="col-md-6 col-md-offset-4">--}}
+                                {{--<button type="submit" class="btn btn-primary">--}}
+                                    {{--Send Password Reset Link--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
                 {{--</div>--}}
-
-                {{--<button type="submit">Send Password Reset Link</button>--}}
             {{--</div>--}}
-        {{--</form>--}}
+        {{--</div>--}}
     {{--</div>--}}
-
-
-    {{--<!-- Scripts -->--}}
-    {{--<script src="{{ asset('js/login.js') }}"></script>--}}
-
-{{--</body>--}}
-{{--</html>--}}
+{{--</div>--}}
+{{--@endsection--}}
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <head>
-    <!-- templatemo 418 form pack -->
-    <!--
-    Form Pack
-    http://www.templatemo.com/preview/templatemo_418_form_pack
-    -->
     <title>Create Account</title>
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+    {{--<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">--}}
     <link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
 </head>
 <body class="templatemo-bg-gray">
@@ -133,100 +126,3 @@
 </body>
 </html>
 
-
-
-
-
-
-{{--Register style--}}
-{{--<link rel="stylesheet" href="css/regstyle.css">--}}
-{{--<body>--}}
-
-{{--<h2>Modal Signup Form</h2>--}}
-
-{{--<button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Sign Up</button>--}}
-
-{{--<div id="id02" class="modal">--}}
-    {{--<span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">×</span>--}}
-    {{--<form class="modal-content animate" method="POST" action="{{ route('register') }}">--}}
-        {{--{{ csrf_field() }}--}}
-        {{--<div class="container">--}}
-
-            {{--<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">--}}
-                {{--<label for="name"><b>Full Name</b></label>--}}
-                {{--<input id="name" type="text" placeholder="Enter your name" name="name" value="{{ old('name') }}" required autofocus>--}}
-                        {{--@if ($errors->has('name'))--}}
-                            {{--<span class="help-block">--}}
-                                {{--<strong>{{ $errors->first('name') }}</strong>--}}
-                            {{--</span>--}}
-                        {{--@endif--}}
-            {{--</div>--}}
-
-            {{--<div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">--}}
-                {{--<label for="username"><b>Username</b></label>--}}
-                {{--<input id="username" type="text" placeholder="Enter username" name="username" value="{{ old('username') }}" required autofocus>--}}
-                {{--@if ($errors->has('name'))--}}
-                    {{--<span class="help-block">--}}
-                                {{--<strong>{{ $errors->first('username') }}</strong>--}}
-                            {{--</span>--}}
-                {{--@endif--}}
-            {{--</div>--}}
-
-
-            {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
-                {{--<div class="col-md-6">--}}
-                    {{--<label for="email"><b>E-Mail Address</b></label>--}}
-                    {{--<input id="email" type="text" placeholder="Enter your e-mail" name="email" value="{{ old('email') }}" required>--}}
-
-                    {{--@if ($errors->has('email'))--}}
-                        {{--<span class="help-block">--}}
-                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
-                                    {{--</span>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
-            {{--</div>--}}
-
-
-            {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
-                {{--<label for="password"><b>Password</b></label>--}}
-                {{--<input id="password" type="password" placeholder="Password" name="password"  required>--}}
-                {{--@if ($errors->has('password'))--}}
-                    {{--<span class="help-block">--}}
-                                {{--<strong>{{ $errors->first('password') }}</strong>--}}
-                            {{--</span>--}}
-                {{--@endif--}}
-            {{--</div>--}}
-
-            {{--<label for="password-confirm"><b>Repeat Password</b></label>--}}
-            {{--<input type="password" placeholder="Repeat Password" id="password-confirm" class="form-control" name="password_confirmation" required>--}}
-
-            {{--<br>--}}
-            {{--<div class="form-group{{ $errors->has('dob') ? ' has-error' : '' }}">--}}
-                {{--<label for="date"><b>Date of Birth</b></label>--}}
-                {{--<input id="dob" type="date" placeholder="Date of Birth" name="dob" value="{{ old('dob') }}" required autofocus>--}}
-                {{--@if ($errors->has('dob'))--}}
-                    {{--<span class="help-block">--}}
-                                {{--<strong>{{ $errors->first('dob') }}</strong>--}}
-                            {{--</span>--}}
-                {{--@endif--}}
-            {{--</div>--}}
-            {{--<br>--}}
-
-
-            {{--<input type="checkbox" checked="checked"> Remember me--}}
-            {{--<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>--}}
-
-            {{--<div class="clearfix">--}}
-                {{--<button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>--}}
-                {{--<button type="submit" class="signupbtn">Sign Up</button>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</form>--}}
-{{--</div>--}}
-
-{{--<!-- Scripts -->--}}
-{{--<script src="{{ asset('js/login.js') }}"></script>--}}
-
-
-{{--</body>--}}
-{{--</html>--}}
